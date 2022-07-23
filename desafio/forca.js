3626ffb4d97e9f07b0265fc4fe2a63020752ad9a
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-07-23 10:08:42 -0300

    ✨ feat: Implementando regras do jogo

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,15 +1,57 @@
+let vidas = 6;
+let palavraCerta = [];
+let chutes = [];
+
 class Forca {
+  constructor(palavra) {
+    this.palavra = palavra;
+    this.preencher();
+  }
+  preencher() {
+    for (var indice = 0; indice < this.palavra.length; indice++) {
+      palavraCerta.push("_");
+    }
+  }
+  chutar(letra) {
+    if (chutes.indexOf(letra) == -1 && letra.length == 1) {
+      chutes.push(letra);
+      var palavraSecreta = this.palavra;
+      var indexLetra = 0;
 
-  chutar(letra) { }
+      for (var indice = 0; indice < palavraSecreta.length; indice++) {
+        if (palavraSecreta[indice] == letra) {
+          indexLetra = [indice];
+          palavraCerta.splice(indexLetra, 1, letra);
+        }
+      }
+      if (indexLetra == 0) {
+        vidas -= 1;
+      }
+    }
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  buscarEstado() {
+    let letrasFaltando = 0;
+    for (var indice = 0; indice < palavraCerta.length; indice++) {
+      if (palavraCerta[indice] == "_") {
+        letrasFaltando++;
+      }
+    }
+    if (letrasFaltando == 0 && vidas > 0) {
+      return "ganhou";
+    } else if (vidas == 0 && letrasFaltando > 0) {
+      return "perdeu";
+    } else {
+      return "aguardando chute";
+    }
+  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+    return {
+      letrasChutadas: chutes, // Deve conter todas as letras chutadas
+      vidas: vidas, // Quantidade de vidas restantes
+      palavra: palavraCerta, // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+    };
   }
 }
 
