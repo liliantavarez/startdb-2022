018480e28469c2d08e8c40f01e994d7ff6b034ab
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-08-26 18:22:25 -0300

    Update forca.js

--- desafio/forca.js
+++ desafio/forca.js
@@ -3,56 +3,59 @@ let palavraCerta = [];
 let chutes = [];
 
 class Forca {
-  constructor(palavra) {
-    this.palavra = palavra;
-    this.preencher();
-  }
-  preencher() {
-    for (var indice = 0; indice < this.palavra.length; indice++) {
-      palavraCerta.push("_");
-    }
-  }
-  chutar(letra) {
-    if (chutes.indexOf(letra) == -1 && letra.length == 1) {
-      chutes.push(letra);
-      var palavraSecreta = this.palavra;
-      var indexLetra = 0;
+	constructor(palavra) {
+		this.palavra = palavra;
+		this.preencher();
+	}
 
-      for (var indice = 0; indice < palavraSecreta.length; indice++) {
-        if (palavraSecreta[indice] == letra) {
-          indexLetra = [indice];
-          palavraCerta.splice(indexLetra, 1, letra);
-        }
-      }
-      if (indexLetra == 0) {
-        vidas -= 1;
-      }
-    }
-  }
+	preencher() {
+		for (var indice = 0; indice < this.palavra.length; indice++) {
+			palavraCerta.push("_");
+		}
+	}
 
-  buscarEstado() {
-    let letrasFaltando = 0;
-    for (var indice = 0; indice < palavraCerta.length; indice++) {
-      if (palavraCerta[indice] == "_") {
-        letrasFaltando++;
-      }
-    }
-    if (letrasFaltando == 0 && vidas > 0) {
-      return "ganhou";
-    } else if (vidas == 0 && letrasFaltando > 0) {
-      return "perdeu";
-    } else {
-      return "aguardando chute";
-    }
-  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+	chutar(letra) {
+		if (chutes.indexOf(letra) == -1 && letra.length == 1 && letra.match(/[a-z]/i)) {
+			chutes.push(letra);
+			var palavraSecreta = this.palavra;
+			var indexLetra = null;
 
-  buscarDadosDoJogo() {
-    return {
-      letrasChutadas: chutes, // Deve conter todas as letras chutadas
-      vidas: vidas, // Quantidade de vidas restantes
-      palavra: palavraCerta, // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-    };
-  }
+			for (var indice = 0; indice < palavraSecreta.length; indice++) {
+				if (palavraSecreta[indice] == letra) {
+					indexLetra = [indice];
+					palavraCerta.splice(indexLetra, 1, letra);
+				}
+			}
+
+			if (indexLetra == null) {
+				vidas -= 1;
+			}
+		}
+	}
+
+	buscarEstado() {
+		let letrasFaltando = 0;
+		for (var indice = 0; indice < palavraCerta.length; indice++) {
+			if (palavraCerta[indice] == "_") {
+				letrasFaltando++;
+			}
+		}
+		if (letrasFaltando == 0 && vidas > 0) {
+			return "ganhou";
+		} else if (vidas == 0 && letrasFaltando > 0) {
+			return "perdeu";
+		} else {
+			return "aguardando chute";
+		}
+	} // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+
+	buscarDadosDoJogo() {
+		return {
+			letrasChutadas: chutes, // Deve conter todas as letras chutadas
+			vidas: vidas, // Quantidade de vidas restantes
+			palavra: palavraCerta, // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+		};
+	}
 }
 
 module.exports = Forca;
